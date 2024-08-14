e9f2a9bb6ec1980bbd31360d61b8a23b5f26b442
Gabriel Silva <gfernandodasilva18@gmail.com>
2024-08-13 21:46:47 -0300

    added tests forms

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -8,4 +8,19 @@ describe("Newsletter Subscribe Form", () => {
     cy.getByData("submit-button").click()
     cy.getByData("success-message").should("exist").contains("tom@aol.com")
   })
+
+
+  it("does NOT allow an invalid email address", () => {
+    cy.getByData("email-input").type("gab")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message").should("not.exist")
+  })
+
+  it("Does not allow registering an already registered email", () => {
+    cy.getByData("email-input").type("john@example.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message").should("exist").contains("already exists. Please use a different email address")
+  })
+
+
 })
