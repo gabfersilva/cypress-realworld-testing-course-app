fb10b7c72f600e5d4cd4a11651539cef23801c15
Gabriel Silva <gfernandodasilva18@gmail.com>
2024-09-05 22:45:44 -0300

    add context courses section

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -16,4 +16,18 @@ describe("home page", () => {
       cy.get("dt").eq(2).contains("Free and Open Source")
     })
   })
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+    it("Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+    it("Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
+  })
 })
